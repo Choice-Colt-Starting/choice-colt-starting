ef163eb45092d5846ecbf5fae02e6a1f7119223a
burggste000 <steedsranch@gmail.com>
2023-08-26 21:55:13 -0600

    FINISHED HEADER FOR TODAY

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import"./header.css";
 import headerLogo from"./headerLogo.png";
+import{NavBtns}from"./NavBtns/NavBtns.jsx";
 
 function Header(){
     return(
-        <div id="headerDiv">
-            <div>
-                <h1 id="headerMyName">Stephen Burggraaf</h1>
+        <div id="headerParentDiv">
+            <div id="topHeaderDiv">
+                <div id="leftHeaderDiv">
+                    <h1 id="headerMyName">Stephen Burggraaf</h1>
+                </div>
+                <div id="rightHeaderDiv">
+                    <img id="headerLogo" src={headerLogo} alt="Choice Colt Starting"/ >
+                </div>
             </div>
-                <img id="headerLogo" src={headerLogo} alt="Choice Colt Starting" />
-            <div id="rightHeaderDiv">
+            <div id="bottomHeaderDiv">
+                <NavBtns />
             </div>
         </div>
     );
---
+++ src/components/Header/NavBtns/NavBtns.jsx
@@ -0,0 +1,21 @@
+import"./navBtns.css";
+
+const NavBtns=()=>{
+    return(
+        <div id="navBtnsParentDiv">
+            <div>
+                <h4 className="NavBtns">Meet Stephen</h4>
+            </div>
+            <div>
+                <h4 className="NavBtns">Contact Us</h4>
+            </div>
+            <div>
+                <h4 className="NavBtns">Horses</h4>
+            </div>
+            <div>
+                <h4 className="NavBtns">Links</h4>
+            </div>
+        </div>
+    );
+};
+export{NavBtns};
