5770c7047b0c5ca3f3592b8315d4e441e586ac6c
burggste000 <steedsranch@gmail.com>
2023-08-31 12:31:37 -0600

    added note for middle component

--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -5,6 +5,7 @@ const Middle=()=>{
     return(
         <div id="middleDiv">
             <img id="steedPic"src={steedPic} alt="logo" />
+            <p>What is Choice Colt Starting? This page will describe the company name.</p>
         </div>
     );
     };
