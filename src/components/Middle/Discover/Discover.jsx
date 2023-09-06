b67d2844d1f1b0007447ff6f31b3717bad6b4990
burggste000 <steedsranch@gmail.com>
2023-09-06 08:20:28 -0600

    CREATED DISCOVER COMPONENT

---
+++ src/components/Middle/Discover/Discover.jsx
@@ -0,0 +1,21 @@
+import"./discover.css";
+
+function Discover(){
+    return(
+        <div>
+            <div>
+                <p>Horses learn from the release of pressure</p>
+            </div>
+            <div>
+                <p>Why use groundwork?</p>
+            </div>
+            <div>
+                <p>Horses with short attention span</p>
+            </div>
+            <div>
+                <p>How do you know when your horse is ready to learn something new</p>
+            </div>
+        </div>
+    );
+}
+export{Discover};
--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -1,11 +1,15 @@
 import"./middle.css";
 import steedPic from "../../Steed_and_horse.png";
+import{Discover}from"./Discover/Discover.jsx";
 
 const Middle=()=>{
     return(
         <div id="middleDiv">
             <img id="steedPic"src={steedPic} alt="logo" />
             <p>What is Choice Colt Starting? This page will describe the company name.</p>
+            <div>
+                <Discover />
+            </div>
         </div>
     );
     };
