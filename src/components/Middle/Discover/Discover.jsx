c4ffb3c4039133347cb6ddf007c10335fc1fd523
burggste000 <steedsranch@gmail.com>
2023-09-06 09:31:07 -0600

    UPDATED DISCOVER DIVS

--- src/components/Middle/Discover/Discover.jsx
+++ src/components/Middle/Discover/Discover.jsx
@@ -3,16 +3,22 @@ import"./discover.css";
 function Discover(){
     return(
         <div id="parentDiscoverDiv">
+            <div id="disDiv0"className="discoverDivs">
+                <p className="disText">Check Progress Of Horses</p>
+            </div>
             <div id="disDiv1"className="discoverDivs">
                 <p className="disText">Horses learn from the release of pressure</p>
             </div>
             <div id="disDiv2"className="discoverDivs">
-                <p className="disText">Why use groundwork?</p>
+                <p className="disText">Visit the ranch</p>
             </div>
             <div id="disDiv3"className="discoverDivs">
-                <p className="disText">Horses with short attention span</p>
+                <p className="disText">Why use groundwork?</p>
             </div>
             <div id="disDiv4"className="discoverDivs">
+                <p className="disText">Horses with short attention span</p>
+            </div>
+            <div id="disDiv5"className="discoverDivs">
                 <p className="disText">How do you know when your horse is ready to learn something new?</p>
             </div>
         </div>
