af490bc2da54b541646f7662216e5478c7b7d697
burggste000 <steedsranch@gmail.com>
2023-09-13 05:37:40 -0600

    still working on training program page

--- src/components/Middle/TrainingProgramPage/TrainingProgramPage.jsx
+++ src/components/Middle/TrainingProgramPage/TrainingProgramPage.jsx
@@ -3,7 +3,38 @@ import"./trainingProgramPage.css";
 const TrainingProgramPage=(props)=>{
     return(
         <div id={props.trainingProgramClicked===false?"none":"parentDiv"}>
-            <p>The Training Program page is currently being developed</p>
+            <div id="programContainer">
+                <div id="daysParentDiv">
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 1-3</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 2</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 3</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 4</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 5</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 6</h4>
+                    </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 7</h4>
+                    </div>
+                </div>
+                <div id="exercisesParentDiv">
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">Roundpenning</h4>
+                    </div>
+                </div>
+                <div id="videoParentDiv">
+                </div>
+            </div>
         </div>
     );
 };
