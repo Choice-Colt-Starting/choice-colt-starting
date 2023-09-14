3e483b2e5a3af2994813fb9d4e8401584ad0c9a0
burggste000 <steedsranch@gmail.com>
2023-09-13 18:54:25 -0600

    CHANGED COLORS FOR TRAINING PROGRAM

--- src/components/Middle/TrainingProgramPage/TrainingProgramPage.jsx
+++ src/components/Middle/TrainingProgramPage/TrainingProgramPage.jsx
@@ -26,17 +26,69 @@ const TrainingProgramPage=(props)=>{
                     <div className="dayDivs">
                         <h4 className="dayText">Day 7</h4>
                     </div>
+                    <div className="dayDivs">
+                        <h4 className="dayText">Day 8</h4>
+                    </div>
                 </div>
                 <div id="exercisesParentDiv">
                     <div className="exercisesDivs">
-                        <h4 className="exercisesText">Roundpenning</h4>
+                        <h4 className="exercisesText">Round Penning</h4>
                     </div>
                     <div className="exercisesDivs">
                         <h4 className="exercisesText">
-                            hi</h4>
+                            Yield Hind Quarters And Backing Up</h4>
                     </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">Lunging</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">
+                            Jeffery's Method</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">Flexing With Snaffle Bit</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">Lunging With Saddle</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">First Ride</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">One Reign Stops</h4>
+                    </div>
+                    {/* <div className="exercisesDivs">
+                        <h4 className="exercisesText">Cruising Lesson</h4>
+                    </div>
+                    <div className="exercisesDivs">
+                        <h4 className="exercisesText">Follow The Fence</h4>
+                    </div> */}
                 </div>
                 <div id="videoParentDiv">
+                <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
+                    <div className="videosDivs">
+                        <a className="videosText" href="https://www.youtube.com">Click Here For Example:</a>
+                    </div>
                 </div>
             </div>
         </div>
