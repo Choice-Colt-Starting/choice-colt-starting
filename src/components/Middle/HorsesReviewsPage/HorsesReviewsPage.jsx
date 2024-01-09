a96862bee492d8cf3e867b4ba7f8a85b72e7ed7b
burggste000 <steedsranch@gmail.com>
2024-01-08 21:53:44 -0700

    ADDED A REVIEW

--- src/components/Middle/HorsesReviewsPage/HorsesReviewsPage.jsx
+++ src/components/Middle/HorsesReviewsPage/HorsesReviewsPage.jsx
@@ -1,5 +1,6 @@
 import"./horsesReviewsPage.css";
 import starsPic from "./images/starsPic.png";
+import kyleePic from "./images/kylee.jpg";
 import sandeePic from "./images/jake.jpeg";
 import nikkiPic from "./images/story.jpeg";
 import nikki2Pic from "./images/roo.jpeg";
@@ -10,6 +11,11 @@ import{HorsesWorldMap}from"./HorsesWorldMap/HorsesWorldMap.jsx";
 function HorsesReviewsPage({ horsesReviewsClicked }){
 
     const horseReviews = [
+        {
+            name: 'Kylee Olson',
+            image: kyleePic,
+            review: 'I took my two year old filly to Stephen in the spring 2023. She definitely had a wild side and I was very nervous she wouldn’t turn out. She had 90 days training with him. Since having her back She is legitimately my favorite horse to ride. She doesn’t even act like what you would expect from a 2 1/2 year old. She is so chill and laid back about everything. I think my most favorite thing about her is she is not buddy sour at all. And she was very much before I took her to Stephen. I think the confidence he builds in them doing all the solo trail/city riding that young that makes a huge difference. She continues to impress me every time I ride her.  I would recommend him to anyone.'
+        },
         {
             name: 'Sandee S',
             image: sandeePic,
